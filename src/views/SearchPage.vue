b02718c1beeeea4d04d271578eea832594683260
Anthony Gonzalez <thomasnrggo@gmail.com>
2023-06-05 00:09:24 -0600

    initialize project, add tw config

---
+++ src/views/SearchPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-100 min-h-screen py-8">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <div class="max-w-3xl mx-auto">
+      <h1 class="text-3xl font-semibold mb-4">Pokemon TCG Tracker</h1>
+      <!-- Add your search bar and card list placeholder here -->
+    </div>
+  </div>
+</template>
