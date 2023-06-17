8176182f9a3f6ece2ef78c866a097a7c07feb74d
Anthony Gonzalez <thomasnrggo@gmail.com>
2023-06-17 16:58:54 -0600

    add env variables

--- src/services/useService.js
+++ src/services/useService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.pokemontcg.io/v2/",
   headers: {
-    "x-api-key": "e415e94a-efa3-4e48-bae3-88160134c596",
+    "x-api-key": process.env.API_KEY,
   },
 });
 
