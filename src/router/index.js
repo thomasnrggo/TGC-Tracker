10536a033bb89d9e09493e9cbf9ce82abff3fcc6
Anthony Gonzalez <thomasnrggo@gmail.com>
2024-01-08 15:54:03 -0600

    Add redirect logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import SearchPage from '../views/SearchPage.vue'
 import DetailPage from '../views/DetailPage.vue'
 import ProfilePage from '../views/ProfilePage.vue'
@@ -36,6 +37,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters.isAuthenticated
+
+  if ((to.path === '/login' || to.path === '/signup') && isAuthenticated) {
+    next({ name: 'home' })
+  } else if (to.path === '/profile' && !isAuthenticated) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
