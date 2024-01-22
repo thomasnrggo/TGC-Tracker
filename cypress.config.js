e9f4c48360b67ed8d2583b5d77588deb39b6d1fd
Anthony Gonzalez <thomasnrggo@gmail.com>
2024-01-22 17:03:48 -0600

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080/',
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
